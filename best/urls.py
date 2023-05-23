0682da9385f006d6143d9f576a82843bf7089274
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-05-23 16:01:13 +0200

    Add back-end, start correcting function that sorts ranking

--- best/urls.py
+++ best/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('game',views.game,name='game')
+    path('game',views.game,name='game'),
+    path('give_points',views.give_points,name='give_points')
 ]
--- best/views.py
+++ best/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Song
 
 # Create your views here.
@@ -9,3 +9,15 @@ def index(request):
 def game(request):
     songs = Song.objects.all()
     return render(request,'game.html', {'songs':songs})
+
+def give_points(request):
+    winner =request.POST['winner_game']
+    second = request.POST['second_game']
+    find_winner = Song.objects.get(title=winner)
+    find_second = Song.objects.get(title=second)
+    find_winner.points += 5
+    find_winner.save()
+    find_second.points += 2
+    find_second.save()
+    
+    return redirect('/')
